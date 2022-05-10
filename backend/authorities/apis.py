fa2df7aa606d7a5ceb79ff1c7be8ae5103bb69c1
R-Future <fhfuture@163.com>
2022-05-10 17:56:36 +0800

    bug fix

--- backend/authorities/apis.py
+++ backend/authorities/apis.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.authtoken.views import APIView
 from rest_framework.permissions import AllowAny
-from django.contrib.auth.models import AnonymousUser, Permission
 
 
 class PermissionAPIView(APIView):
@@ -9,10 +8,6 @@ class PermissionAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         has_perm = False
-        if request.user is not None and not isinstance(request.user, AnonymousUser):
-            try:
-                issue_perm = Permission.objects.get(codename='add_issue')
-                has_perm = request.user.has_perm(issue_perm)
-            except Permission.DoesNotExist:
-                has_perm = False
+        if request.user is not None:
+            has_perm = request.user.has_perm('books.add_issue')
         return Response({'has_issue_perm': has_perm})
--- fileservice/tasks/celery_config.py
+++ fileservice/tasks/celery_config.py
@@ -7,8 +7,8 @@ class CeleryConfig:
     broker_url = f'pyamqp://{os.getenv("RABBITMQ_USER")}:{os.getenv("RABBITMQ_PASS")}@{os.getenv("RABBITMQ_HOST")}:{os.getenv("RABBITMQ_PORT")}/{os.getenv("RABBITMQ_VHOST")}'
     result_backend = f'redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/{os.getenv("REDIS_DB")}'
     include = ['tasks.main']
-    task_time_limit = 60 * 60  # hard time limit 1 hour
-    task_soft_time_limit = 60 * 60
+    task_time_limit = 60 * 60 * 2  # hard time limit 1 hour
+    task_soft_time_limit = 60 * 60 * 2
     task_default_rate_limit = 10  # allow 10 tasks per minute
     worker_max_memory_per_child = 256000  # 256MB
     worker_cancel_long_running_tasks_on_connection_loss = True
