8856e5c12e79cb7025a50dde812f476acdbfa53f
R-Future <fhfuture@163.com>
2022-08-18 13:42:35 +0800

    update nginx conf

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
     },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'd-book'),
-        'USER': os.getenv('DB_USER', 'dev'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'dev@2021'),
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {'charset': 'utf8mb4'}
