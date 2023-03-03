52e98765787c1db691c3300f4462775b026a8847
R-Future <fhfuture@163.com>
2023-03-04 01:48:24 +0800

    optimization

--- server/notifications/consumers.py
+++ server/notifications/consumers.py
@@ -4,8 +4,11 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 class NotificationConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
-        self.user_id = self.scope['url_route']['kwargs']['user_id']
-        self.group_name = f'notification_{self.user_id}'
+        user = self.scope['user']
+        if bool(user is None or not user.is_authenticated()):
+            await self.close()
+
+        self.group_name = f'notification_{user.id}'
 
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
